6c3031fa1fc0ec9a8c713e3cea5726bc9bdd20b2
Umar Siddiqui <umar@treeni.com>
2015-11-05 15:01:51 +0530

    Current graph :: tatical priorty added

--- app/serializers/answers_serializers/current_graph_serializer.rb
+++ app/serializers/answers_serializers/current_graph_serializer.rb
@@ -13,32 +13,43 @@ module AnswersSerializers
     end
 
     def gen_list(stage)
-      object.select { |answer| answer.value == stage }.map(&:section).map do |sec|
-        next function_section(sec) if type_function?(sec.name)
-        readiness_section(sec)
+      object.select { |answer| answer.value == stage }.map do |ans|
+        next function_section(ans) if type_function?(ans.section.name)
+        readiness_section(ans)
       end
     end
 
-    def function_section(section)
+    def function_section(ans)
       {
-        label: section.business_function.acronym,
-        type: section.name.split(' ').first.downcase,
-        full_label: section.business_function.name
+        label: ans.section.business_function.acronym,
+        type: ans.section.name.split(' ').first.downcase + ' '+ priorty(ans.score),
+        full_label: ans.section.business_function.name
       }
     end
 
-    def readiness_section(section)
+    def readiness_section(ans)
       {
-        label: section.name.split(' ').map { |t| t[0] }.join('')
+        label: ans.section.name.split(' ').map { |t| t[0] }.join('')
           .gsub(/\W/, '')[0..2].upcase,
-        type: 'Readiness'.downcase,
-        full_label: section.name
+        type: 'Readiness'.downcase + ' '+ priorty(ans.score),
+        full_label: ans.section.name
       }
     end
 
     def type_function?(section_name)
       ['Strategic Integration', 'Cultural Integration', 'Operational Integration']
         .include? section_name
+    end
+
+    private
+
+    def priorty(score)
+      value = score.try(:value)
+      return '' if value.nil?
+      return 'low' if value <= 2 && value >= 0
+      return 'medium' if value <= 6 && value >= 3
+      return 'high' if value <= 12 && value >= 8
+      ''
     end
   end
 end
