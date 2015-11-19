f0eb6a527fef4a274be20ece8db0224017b65719
SnehMohite <sneh.mohite31@gmail.com>
2015-11-19 14:02:12 +0530

    Instance:: Enviornment for Testing Instance

---
+++ config/deploy/testing.rb
@@ -0,0 +1,12 @@
+server 'ungc.cloudapp.net', user: 'azureuser', roles: %w{app db web}, primary: true
+set :deploy_to, '/var/www/ungc-lead'
+set :branch, "develop"
+
+server 'ungc.cloudapp.net',
+  user: 'azureuser',
+  roles: %w{web app},
+  ssh_options: {
+    keys: %w(/home/webonise/.ssh/id_rsa),
+    forward_agent: false,
+    auth_methods: %w(publickey password)
+  }
