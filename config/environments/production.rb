bda3e756dd0aeb6fa5b537a9988449aab0a24129
SnehMohite <sneh.mohite31@gmail.com>
2015-11-19 19:14:50 +0530

    Admin:: Footer Loader Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,14 +76,13 @@ Rails.application.configure do
 
   # Configure Mail
   config.action_mailer.default_url_options = { :host => '52.7.147.7' }
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: 'exceptions.treeni@gmail.com',
-    password: 'Treeni@123',
-    authentication: 'plain',
-    enable_starttls_auto: true
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'email@weboapps.com',
+    :password             => 'weboqa6186',
+    :authentication       => "plain",
+    :enable_starttls_auto => true
   }
 end
