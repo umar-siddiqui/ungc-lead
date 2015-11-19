e336aea4ae914f7fdbfc3d4773f772fc69b156a3
SnehMohite <sneh.mohite31@gmail.com>
2015-11-19 13:00:15 +0530

    Admin:: invite mail reset password footer changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: 'email@weboapps.com',
-    password: 'weboqa6186',
+    user_name: 'exceptions.treeni@gmail.com',
+    password: 'Treeni@123',
     authentication: 'plain',
     enable_starttls_auto: true
   }
