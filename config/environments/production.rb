99f2945a7645848f070d00429511268c1ee362b3
Ketan Ghumatkar <ketan@treeni.com>
2015-12-07 17:44:08 +0530

    Mailing Configuration :: Change mailing default url for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Configure Mail
-  config.action_mailer.default_url_options = { :host => '52.7.147.7' }
+  config.action_mailer.default_url_options = { :host => 'roadmapselfassessment.unglobalcompact.org' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
