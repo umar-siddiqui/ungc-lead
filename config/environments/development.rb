026fb1427a2022a15f4f5cc3424a0fee723facd9
Ketan Ghumatkar <ketan@treeni.com>
2015-12-07 16:43:25 +0530

    Mailing Configuration :: Configuration ungc mailing credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -39,12 +39,15 @@ Rails.application.configure do
   # Configure Mail
   config.action_mailer.default_url_options = { :host => 'ungc.cloudapp.net' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => 'email@weboapps.com',
-    :password             => 'weboqa6186',
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+    :port                 => 465,
+    :user_name            => 'self_assessment@unglobalcompact.org',
+    :password             => 's3lf4ss3ssm3nt',
+    :authentication       => :login,
+    :enable_starttls_auto => true,
+    :ssl                  => true
   }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => 'email@weboapps.com',
-    :password             => 'weboqa6186',
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+    :port                 => 465,
+    :user_name            => 'self_assessment@unglobalcompact.org',
+    :password             => 's3lf4ss3ssm3nt',
+    :authentication       => :login,
+    :enable_starttls_auto => true,
+    :ssl                  => true
   }
 end
