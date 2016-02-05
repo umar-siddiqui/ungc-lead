96417e49738cebb5d4ffe8a4b8c2df0403a93e74
Umar Siddiqui <umar@treeni.com>
2016-02-05 20:11:20 +0530

    Sections : Validation of function getting added only if 1st 2 sections answered

--- app/assets/javascripts/angular_apps/sections/controllers/sections.controller.js
+++ app/assets/javascripts/angular_apps/sections/controllers/sections.controller.js
@@ -11,11 +11,11 @@
   function SectionsController($scope, $window, $state, $http, $stateParams) {
 
     $scope.markDone = markDone;
+    $scope.checkIfSectionsDoneForFS = checkIfSectionsDoneForFS;
 
     function init(){
       $scope.errors = [];
       getSectionTree();
-      $scope.canConclude = true;
     }
 
     function getSectionTree(){
@@ -65,15 +65,27 @@
     }
 
     $scope.fetchQuestions = function (section){
+      $scope.errors = [];
+      if(section.name == 'Functional Assesment' && !checkIfSectionsDone([$scope.sections[0], $scope.sections[1]], true)){
+        $scope.errors
+              .push('Please complete the Readiness and Functional Priority sections before attempting Functional Assessment Sections');
+        return $scope.$apply();
+      }
+
       $scope.section = section;
       $state.go('sections.questions', { section_id: section._id });
     };
 
-    function checkIfSectionDone(sections){
+    function checkIfSectionsDoneForFS(section) {
+      return checkIfSectionsDone([$scope.sections[0], $scope.sections[1]], true);
+    }
+
+    function checkIfSectionsDone(sections, flag){
       angular.forEach(sections, function(section){
-        if (!section.submitted) { return $scope.canConclude = false; };
-        checkIfSectionDone(section.sections)
+        if (!section.submitted) { return flag = false; };
+        checkIfSectionsDone(section.sections, flag)
       });
+      return flag;
     }
 
     function markDone(sections){
@@ -92,14 +104,16 @@
       var result = _.countBy(section.sections, function(section) {
         return section.submitted;
       });
-      $scope.canConclude = angular.isUndefined(result[true]) || result[true] < no ? false : true;
+      return angular.isUndefined(result[true]) || result[true] < no ? false : true;
     }
 
     $scope.concluded = function(){
-      checkIfSectionDone([$scope.sections[0], $scope.sections[1]]);
-      checkIfRequiredNumberDone($scope.sections[2], 3);
+      var sectionsDone = checkIfSectionsDone([$scope.sections[0], $scope.sections[1]], true);
+      var requiredNumberDone = checkIfRequiredNumberDone($scope.sections[2], 3);
       $scope.errors = [];
-      if(!$scope.canConclude) return $scope.errors.push('Please complete the Readiness and Functional Priority sections and at least 3 Functional Assessments before concluding.');
+      if(!sectionsDone || !requiredNumberDone)
+        return $scope.errors
+                     .push('Please complete the Readiness and Functional Priority sections and at least 3 Functional Assessments before concluding.');
       updateReportState();
     }
 
